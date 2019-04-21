ce4ccc6378e8f051976488c4b9825e54af1332cd
taufiq.pirjade <taufiq2215@gmail.com>
2019-04-21 15:20:04 +0530

    Changes in SOL file and respective bin and abi

--- register.sol
+++ register.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.1;
+pragma solidity ^0.5.7;
 
 contract Register {
 
@@ -19,5 +19,23 @@ contract Register {
     function getInfo() public view returns (bytes32) {
         return info;
     }
-
+    
+    function bytes32ArrayToString (bytes32[] memory data) public pure returns (string memory) {
+        bytes memory bytesString = new bytes(data.length * 32);
+        uint urlLength;
+        for (uint i=0; i<data.length; i++) {
+            for (uint j=0; j<32; j++) {
+                byte char = byte(bytes32(uint(data[i]) * 2 ** (8 * j)));
+                if (char != 0) {
+                    bytesString[urlLength] = char;
+                    urlLength += 1;
+                }
+            }
+        }
+        bytes memory bytesStringTrimmed = new bytes(urlLength);
+        for (uint i=0; i<urlLength; i++) {
+            bytesStringTrimmed[i] = bytesString[i];
+        }
+        return string(bytesStringTrimmed);
+    }
 }
